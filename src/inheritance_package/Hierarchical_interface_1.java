3ff6d912ea65593a7f38a4672c740c32cfa534fa
Abhishek K <kabhik2898@gmail.com>
2022-09-21 08:24:58 +0530

    Hierarchical interface added

---
+++ src/inheritance_package/Hierarchical_interface_1.java
@@ -0,0 +1,15 @@
+package inheritance_package;
+
+public class Hierarchical_interface_1 extends Multi{
+	
+	public static void main(String[] args) {
+		
+		Hierarchical_interface_1 hli = new Hierarchical_interface_1();
+		
+		int num1 = 22;
+		int num2 = 10;
+		
+		System.out.println(hli.multi(num1, num2));
+	}
+
+}
---
+++ src/inheritance_package/hierarchical_interface_2.java
@@ -0,0 +1,15 @@
+package inheritance_package;
+
+public class hierarchical_interface_2 extends Multi{
+	
+	public static void main(String[] args) {
+		
+		hierarchical_interface_2 hli = new hierarchical_interface_2();
+		
+		int a = 10;
+		int b = 33;
+		
+		System.out.println(hli.multi(a, b));
+	}
+
+}
